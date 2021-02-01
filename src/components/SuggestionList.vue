d1c8b5d689400430a0670f23fcdb34f82b8dd7ac
thomascarleberg <thomascarleberg@gmail.com>
2021-02-01 11:06:32 +0100

    lez changez

--- src/components/SuggestionList.vue
+++ src/components/SuggestionList.vue
@@ -1,24 +1,29 @@
 <template>
   <ul>
-    <li 
-      v-for="color in colors" 
-      :key="color"
-      @click="$emit('click', color)"
-    >{{color}}</li>
+    <li v-for="color in colors" :key="color" @click="$emit('click', color)">
+      {{ color }}
+    </li>
   </ul>
 </template>
 
 <script>
 export default {
-  props: ['match'],
-  data(){ return {
-    colors: ['red','green','blue'] // Change this to a vuex getter that filters based on this.match
-  }},
-}
+  props: ["match"],
+  data() {
+    return {
+      //colors: ['red','green','blue'] // Change this to a vuex getter that filters based on this.match
+    };
+  },
+  computed: {
+    colors() {
+      return this.$store.getters.firstN(4);
+    },
+  },
+};
 </script>
 
 <style scoped lang="scss">
-ul{
+ul {
   position: absolute;
 }
 </style>
